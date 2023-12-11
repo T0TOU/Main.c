8bedf7c868d27d33a61d249fb7d6cf9929af7dc2
T0TOU <153504521+T0TOU@users.noreply.github.com>
2023-12-11 18:14:52 +0800

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("ºÆºÆ\n");
+    char ch = 'A';
+    printf("%c\n", ch); 
+    int age = 16;
+    printf("%d\n", age);
+    double xiao = 3.1415926;
+    printf("%lf\n", xiao);
+    float hx = 5.0;
+    printf("%f\n", hx);
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(double));
+    printf("%d\n", sizeof(float));
+    return 0;  
+}
