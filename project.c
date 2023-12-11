8d1d32c4c43179fa21298d65267a3334e9504aee
T0TOU <153504521+T0TOU@users.noreply.github.com>
2023-12-11 18:46:28 +0800

    Add files via upload

--- project.c
+++ project.c
@@ -1,10 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include<string.h>
 int main()
 {
-	const int num = 8;  //const--常属性（固定一个变量为固定值）const修饰的常变量
-	printf("%d\n",num);
-	int arr[2] = { 0 };    //"2"所在的位置是必须用常量的，而const  num无法运行，所以num不完全是常量,
-	                       //const int num是一个固定了的变量.
+	char arr1[] = "abcde";	
+	char arr2[] = { 'a','b','c',0};
+	char arr3[] = "hello,bit";
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%s\n", arr3);
+	printf("%d\n", strlen(arr1)); //strlen---string(一串）length(n，长，长度；long的变形加th）
+	printf("%d\n", strlen(arr2)); //strlen 计算一串字符长度的
+	printf("%d\n", strlen(arr3));
+	printf("c;\test\32\test.c\n");// \t--水平制表符键盘上tab键	转意字符
+	printf("c:\\test\\32\\test.c\n"); //通过在转意义的字符前加"/"使其与斜杠一起变为普通词
+	printf("(are you ok??)\n"); //老版本的编译器会有三字母词 例：“？？+（”=[ 在问号前加 “/"结决问题，/？的由来
+	printf("%c\n",'\''); //用\可以隔开相和成为功能的字符，%c打印字符，%d打印整形  
+	printf("%d\n", strlen("c:\test\32\test.c"));// \32作为八进制代表的那个十进制数，作为ASCII码值，对应的字符
+	printf("%d\n", strlen("c:\\test\\32\\test.c"));//32 --> 011010 -->26--->做为ASKII码值代表的字符
+	printf("%c\n", '\141');        //  \ddd ddd表示1~3个八进制数  \xdd dd表示2个十六进制数字
 	return 0;
 }
